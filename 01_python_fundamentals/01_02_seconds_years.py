b7544ff5153d8f08724408431492449c40d71307
Soopernoodle <bao0ha0@gmail.com>
2020-05-06 11:54:40 -0400

    First labs

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -4,3 +4,9 @@ Move the code you previously wrote to calculate how many seconds are in a year i
 Then execute it as a script to see the output printed to your console.
 
 '''
+seconds = 60
+minutes = 60
+hours = 24
+year = 256
+x = seconds * minutes * hours * year
+print(str(x) + " seconds")
--- 01_python_fundamentals/01_07_area_perimeter.py
+++ 01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,9 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+
+width = 2.4
+length = 6.4
+area = width * length
+perimeter = (width + length) * 2
+print (str(area) + "\n" + str(perimeter))
